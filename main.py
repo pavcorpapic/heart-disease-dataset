2812c4cfd75548a868799751e53e10e59cd40336
pavcorpapic <148006469+pavcorpapic@users.noreply.github.com>
2023-12-26 15:39:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,3 +18,24 @@ occurrences = (data == '?').sum() #Sum of '?' occurrences
 prec = (1 - occurrences/len(data.iloc[:,1]))*100 #Percentage of non-null values
 
 data.replace('?', np.nan, inplace=True) #inplace=True ensures that the modification is applied to the original dataset
+
+for i in range(3, 13):
+    data.iloc[:,i]=data.iloc[:,i].astype(float) #Converting the data types to float
+
+#Replacing the NaN values with mean or median values
+data['trestbps'].fillna(data['trestbps'].mean(), inplace=True)
+data['oldpeak'].fillna(data['oldpeak'].median(), inplace=True)
+data['thalach'].fillna(data['thalach'].mean(), inplace=True)
+
+#Dropping the rows with NaN values
+data.dropna(subset=['fbs'], inplace=True)
+data.dropna(subset=['exang'], inplace=True)
+data.dropna(subset=['restecg'], inplace=True)
+
+data.reset_index(drop=True) #Resetting the indices
+
+data['chol'].replace(0, np.nan, inplace=True)
+data['chol'].fillna(data['chol'].median(), inplace=True)
+
+classes = data.iloc[:, data.shape[1]-1]
+value = np.unique(classes)
